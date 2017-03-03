cea81ac83a37025bf137d49a82ba0eb06684ba5c
Vlad-Apostolov <udc.aus@gmail.com>
2017-03-03 22:00:54 +1100

    Modified ping.cpp. Added new source files and Eclipse project files.

---
+++ Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lboost_program_options -lboost_chrono -lboost_date_time -lboost_filesystem -lboost_regex -lboost_atomic -lboost_log -lpthread -lboost_thread -lboost_system
+
---
+++ main.cpp
@@ -0,0 +1,127 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 27/02/2017
+ *      Author: vlad
+ */
+
+#include <signal.h>
+#include <map>
+#include <boost/thread.hpp>
+#include <boost/program_options.hpp>
+#include <boost/iostreams/device/file.hpp>
+#include <boost/iostreams/stream.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/asio.hpp>
+#include <boost/shared_ptr.hpp>
+#include <iostream>
+
+#include "simpleLogger.h"
+#include "ping.h"
+
+using namespace std;
+using namespace boost::program_options;
+using namespace boost::iostreams;
+using namespace boost::filesystem;
+
+// RtiProxyClient logging control variables
+static bool consoleLogging = false;			// when set to true, enables logging to console
+static string logLevel = "info";			// RtiProxyClient logging threshold level: trace|debug|info|warning|error|fatal
+static string logFileName = "";				// non empty string enables logging into a file
+static string pingServerName = "";
+static variables_map variablesMap;
+static options_description optionsDescription{"Options"};
+
+
+static shared_ptr<boost::asio::io_service> processingScheduler;				// processing scheduler
+
+static void stopProcessing(int dummy)
+{
+	LOG_TRACE << __PRETTY_FUNCTION__;
+	processingScheduler->stop();
+}
+
+static void configureLogger()
+{
+	using namespace boost::log::trivial;
+	const char* logFile = logFileName.empty() ? NULL : logFileName.c_str();
+	severity_level level;
+
+	if (logLevel == "trace")
+		level = trace;
+	else if (logLevel == "debug")
+		level = debug;
+	else if (logLevel == "info")
+		level = info;
+	else if (logLevel == "warning")
+		level = warning;
+	else if (logLevel == "error")
+		level = boost::log::trivial::error;
+	else if (logLevel == "fatal")
+		level = fatal;
+	else {
+		cout << "Unexpected logging level!" << endl;
+		exit(EXIT_FAILURE);
+	}
+	configureLogger(level, consoleLogging, logFile);
+}
+
+static void parseCommandLine(int argc, char *argv[])
+{
+	try	{
+		optionsDescription.add_options()
+			("help,h", "Help screen")
+			("logLevel,l", value<string>(&logLevel)->default_value("info"), "Logging severity threshold: trace|debug|info|warning|error|fatal")
+			("consoleLog,c", "Enable console logging")
+			("logFileName,f", value<string>(&logFileName)->default_value(""), "File name for logging")
+			("pingServerName,p", value<string>(&pingServerName)->required(), "Server name to ping");
+
+
+		command_line_parser commandLineParser{argc, argv};
+		commandLineParser.options(optionsDescription);
+		parsed_options parsedOptions = commandLineParser.run();
+
+		store(parsedOptions, variablesMap);
+		notify(variablesMap);
+
+		if (variablesMap.count("help")) {
+			cout << optionsDescription << endl;
+			exit(EXIT_SUCCESS);
+		}
+
+		if (variablesMap.count("consoleLog"))
+			consoleLogging = true;
+	} catch (const boost::program_options::required_option& e) {
+        if (variablesMap.count("help")) {
+            cout << optionsDescription << endl;
+			exit(EXIT_FAILURE);
+        } else {
+            throw e;
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+	std::size_t pingReplies = 0;
+
+	shared_ptr<boost::asio::io_service> io_service(new boost::asio::io_service);
+	processingScheduler = io_service;
+	signal(SIGINT, stopProcessing);
+	parseCommandLine(argc, argv);
+	configureLogger();
+
+	try
+	{
+		pinger ping(processingScheduler, pingServerName.c_str());
+		processingScheduler->run();
+		pingReplies = ping.getReplies();
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << "Exception: " << e.what() << std::endl;
+	}
+
+	LOG_TRACE << "ping replies: " << pingReplies;
+	return EXIT_SUCCESS;
+}
